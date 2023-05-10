9f828f88299663670fa994b61f1b221e5b2cc635
Ismael Lopez Hernandez <ismael.lopez.hernandes@gmail.com>
2023-05-10 15:41:59 -0500

    update test

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -19,42 +19,68 @@ suite('Unit Tests', function(){
             assert.equal(convertHandler.getReturnUnit(input), 'lbs')
         })
         test('#4 return invalid unit', () => {
+            let input = "kg"
+            assert.equal(convertHandler.getReturnUnit(input), 'lbs')
 
         })
         test('#5 return invalid double division', () => {
+            let input = "kg"
+            assert.equal(convertHandler.getReturnUnit(input), 'lbs')
 
         })
         test('#6 read a fraction "decimal" number', () => {
+            let input = "kg"
+            assert.equal(convertHandler.getReturnUnit(input), 'lbs')
 
         })
         test('#7 read a fraction number', () => {
+            let input = "kg"
+            assert.equal(convertHandler.getReturnUnit(input), 'lbs')
 
         })
         test('#8 return the string spell of converted unit', () => {
+            let input = "kg"
+            assert.equal(convertHandler.getReturnUnit(input), 'lbs')
 
         })
         test('#9 return default numerical when no present', () => {
+            let input = "kg"
+            assert.equal(convertHandler.getReturnUnit(input), 'lbs')
 
         })
         test('#10 convert gal to L', () => {
+            let input = "kg"
+            assert.equal(convertHandler.getReturnUnit(input), 'lbs')
 
         })
         test('#11 convert L to gal', () => {
+            let input = "kg"
+            assert.equal(convertHandler.getReturnUnit(input), 'lbs')
 
         })
         test('#12 convert mi to km', () => {
+            let input = "kg"
+            assert.equal(convertHandler.getReturnUnit(input), 'lbs')
 
         })
         test('#13 convert km to mi', () => {
+            let input = "kg"
+            assert.equal(convertHandler.getReturnUnit(input), 'lbs')
 
         })
         test('#14 convert lbs to kg', () => {
+            let input = "kg"
+            assert.equal(convertHandler.getReturnUnit(input), 'lbs')
 
         })
         test('#15 convert kg to lbs', () => {
+            let input = "kg"
+            assert.equal(convertHandler.getReturnUnit(input), 'lbs')
 
         })
         test('#16 read decimal number', () => {
+            let input = "kg"
+            assert.equal(convertHandler.getReturnUnit(input), 'lbs')
 
             
         })
